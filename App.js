97a837053f639a984ebbd23b11e09798491ae3ec
juancamilorivas <bogota.diamond@hotmail.com>
2023-03-25 11:24:54 -0500

    Practice

--- App.js
+++ App.js
@@ -2,27 +2,11 @@ import { View, Text } from "react-native";
 import React, { useEffect, useState } from "react";
 
 const App = () => {
-  const [posts, setPosts] = useState([]);
-
-  useEffect(() => {
-    fetch("https://jsonplaceholder.typicode.com/posts")
-      .then((res) => {
-        return res.json();
-      })
-      .then((res) => {
-        setPosts(res);
-      });
-  }, []);
-
   return (
-    <>
-      {posts && posts.map((post) => (
-        <View key={post.id}>
-          <Text>{post.title}</Text>
+    <View>
+      <Text>Hello world</Text>
         </View>
-      ))}
-    </>
-  );
+  )
 };
 
 export default App;
